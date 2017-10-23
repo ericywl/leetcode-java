df3d3e7e6722a0e19e5344f0ca924f3bf53204b4
ericywl <midnight2903@gmail.com>
2017-10-24 00:10:31 +0800

    removed factorial from ClimbingStairs

--- src/Q0070_ClimbingStairs.java
+++ src/Q0070_ClimbingStairs.java
@@ -19,11 +19,6 @@ public class Q0070_ClimbingStairs {
         return sum;
     }
 
-    private static int factorial(int n) {
-        if (n == 1) return 1;
-        return n * factorial(n - 1);
-    }
-
     /*
         1 - 1
         2 - 11, 2
