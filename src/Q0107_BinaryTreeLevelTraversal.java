1ab9421c9fdef491857992657db6b695327a1af5
ericywl <midnight2903@gmail.com>
2017-10-26 00:27:29 +0800

    finished SortedArrayToTree

--- src/Q0107_BinaryTreeLevelTraversal.java
+++ src/Q0107_BinaryTreeLevelTraversal.java
@@ -12,15 +12,9 @@ public class Q0107_BinaryTreeLevelTraversal {
         }
     }
 
+    // Use BFS
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         return new ArrayList<>();
     }
 
-    private List<Integer> currentLevel(TreeNode root) {
-        List<Integer> outputList = new ArrayList<>();
-        outputList.add(root.left.val);
-        outputList.add(root.right.val);
-
-        return outputList;
-    }
 }
---
+++ src/Q0108_SortedArrayToTree.java
@@ -0,0 +1,34 @@
+public class Q0108_SortedArrayToTree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) return null;
+
+        return node(nums, 0, nums.length - 1);
+    }
+
+    private static TreeNode node(int[] nums, int low, int high) {
+        if (low > high) return null;
+        if (low == high) return new TreeNode(nums[low]);
+
+        int mid = (high + low) / 2;
+
+        TreeNode parent = new TreeNode(nums[mid]);
+        parent.left = node(nums, low, mid - 1);
+        parent.right = node(nums, mid + 1, high);
+
+        return parent;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
