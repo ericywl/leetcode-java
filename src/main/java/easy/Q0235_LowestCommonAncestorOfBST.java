967fbc8efd9b5c8c6d9c3b3e8aaf79f313b516a1
ericywl <midnight2903@gmail.com>
2017-12-30 01:36:07 +0800

    finished LCA of BST

---
+++ src/main/java/easy/Q0235_LowestCommonAncestorOfBST.java
@@ -0,0 +1,26 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q0235_LowestCommonAncestorOfBST {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left, p, q);
+
+        if (root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+
+        return root;
+    }
+}
